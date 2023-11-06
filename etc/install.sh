5b6b4374f7d225b2c7f0ce0c6b2de66456c8137e
ea7kir <mikenaylorspain@icloud.com>
2023-11-06 17:49:47 +0100

    231106.1749

--- etc/install.sh
+++ etc/install.sh
@@ -13,22 +13,24 @@ if [ $? != 0 ]; then
   exit
 fi
 
-hostname | grep -q rxtouch
+hostname | grep -q pi4 #rxtouch
 if [ $? != 0 ]; then
   echo Install must be performed on host rxtouch
   exit
 fi
 
-while true; do
-    read -p "Install q100receiver using Go version $GOVERSION (y/n)? " answer
-    case ${answer:0:1} in
-        y|Y ) break;;
-        n|N ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+#while true; do
+#    read -p "Install q100receiver using Go version $GOVERSION (y/n)? " answer
+#    case ${answer:0:1} in
+#        y|Y ) break;;
+#        n|N ) exit;;
+#        * ) echo "Please answer yes or no.";;
+#    esac
+#done
 
-mkdir /home/pi/Q100
+#mkdir /home/pi/Q100
+
+echo "\n###################################################\n"
 
 echo Updateing Pi OS
 sudo apt update
@@ -36,9 +38,13 @@ sudo apt -y full-upgrade
 sudo apt -y autoremove
 sudo apt clean
 
+echo "\n###################################################\n"
+
 echo Running rfkill # not sure if this dupicates config.txt
 rfkill block 0
 rfkill block 1
+
+echo "\n###################################################\n"
 
 echo Making changes to config.txt
 
@@ -51,11 +57,15 @@ echo -e "\ndtoverlay=disable-bt" >> /boot/config.txt
 echo EXPERIMENTAL: raspi-config, select System / Audio, choose 1
 echo -e "\ndtparam=audio=off" >> /boot/config.txt
 
-echo Installing GIT
-sudo apt -y install git
+#echo Installing GIT
+#sudo apt -y install git
+
+echo "\n###################################################\n"
 
 echo Adding go path to .profile
 echo -e '\n\nexport PATH=$PATH:/usr/local/go/bin\n\n' >> /home/pi/.profile
+
+echo "\n###################################################\n"
 
 echo Installing Go $GOVERSION
 GOFILE=go$GOVERSION.linux-arm64.tar.gz
@@ -65,16 +75,27 @@ sudo wget https://go.dev/dl/$GOFILE
 sudo tar -C /usr/local -xzf $GOFILE
 cd
 
+echo "\n###################################################\n"
+
 echo Installing gioui dependencies
 sudo apt install gcc pkg-config libwayland-dev libx11-dev libx11-xcb-dev libxkbcommon-x11-dev libgles2-mesa-dev libegl1-mesa-dev libffi-dev libxcursor-dev libvulkan-dev
 
-echo Cloning q100receiver to /home/pi/Q100
-cd /home/pi/Q100
-git clone https://github.com/ea7kir/q100receiverr.git
-cd
+#echo Cloning q100receiver to /home/pi/Q100
+#cd /home/pi/Q100
+#git clone https://github.com/ea7kir/q100receiverr.git
+#cd
+
+echo "\n###################################################\n"
 
 echo Install the No Video caption
 sudo cp /home/pi/Q100/q100receiver/etc/NoVideo.jpg /usr/share/rpd-wallpaper
+
+echo "\n###################################################\n"
+
+echo Install longmynd dependencies
+sudo apt install make gcc libusb-1.0-0-dev libasound2-dev
+
+echo "\n###################################################\n"
 
 echo Cloning longmynd to /home/pi/Q100
 cd /home/pi/Q100
@@ -85,14 +106,17 @@ mkfifo longmynd_main_status
 mkfifo longmynd_main_ts
 cd
 
+echo "\n###################################################\n"
+
 echo Copying q100receiver.service
-cd /home/pi/Q100/etc
+cd /home/pi/Q100/q100receiver/etc
 sudo cp q100receiver.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/q100receiver.service
 sudo systemctl daemon-reload
 cd
 
-echo "\n
+echo "\n###################################################\n"
+
 INSTALL HAS COMPLETED
    after rebooting, build and auto exec...
 
